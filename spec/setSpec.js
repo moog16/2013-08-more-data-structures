2f65a0a992d5d42b9e3722addf50e20816dfa3b9
Hack Reactor Students <github@hackreactor.com>
2013-08-09 15:42:02 -0700

    made a working set using objects with some test

--- spec/setSpec.js
+++ spec/setSpec.js
@@ -10,4 +10,51 @@ describe("set", function() {
     expect(set.contains).toEqual(jasmine.any(Function));
     expect(set.remove).toEqual(jasmine.any(Function));
   });
+
+  it("should contain value 'a' after adding 'a' to set", function() {
+    set.add("a");
+    expect(set.contains('a')).toEqual(true);
+  });
+
+  it("should not contain value 'd' after adding 'a', 'b', and 'c' to set", function() {
+    set.add("a");
+    set.add("b");
+    set.add("c");
+    expect(set.contains('d')).toEqual(false);
+  });
+
+  it("should contain value 'c' after adding 'a', 'b', and 'c' to set", function() {
+    set.add("a");
+    set.add("b");
+    set.add("c");
+    expect(set.contains('b')).toEqual(true);
+    expect(set.contains('c')).toEqual(true);
+  });
+
+  it("should not contain 'c' after removing 'c' from set", function() {
+    set.add("a");
+    set.add("b");
+    set.add("c");
+    set.remove('c');
+
+    expect(set.contains('c')).toEqual(false);
+  });
+
+  it("should not contain 'c' after removing 'c' from set", function() {
+    set.add("a");
+    set.add("b");
+    set.add("c");
+    set.remove('c');
+
+    expect(set.contains('c')).toEqual(false);
+  });
+
+  it("should not contain 'c' after removing 'c' from set", function() {
+    set.add("a");
+    set.add("b");
+    set.add("c");
+    set.remove('c');
+
+    expect(set.contains('c')).toEqual(false);
+  });
 });
--- src/set.js
+++ src/set.js
@@ -1,16 +1,23 @@
 var makeSet = function(){
   var set = Object.create(setPrototype); // fix me
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(){
+setPrototype.add = function(str){
+  if(!this.contains(str)) {
+    this._storage[str] = str;
+  }
 };
 
-setPrototype.contains = function(){
+setPrototype.contains = function(str){
+  return this._storage[str] !== undefined ? true : false;
 };
 
-setPrototype.remove = function(){
+setPrototype.remove = function(str){
+  if(this.contains(str)) {
+    delete this._storage[str];
+  }
 };
