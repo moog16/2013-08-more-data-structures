166fbe43e7cdbaefcbf34d13a1dde5110c939118
Hack Reactor Students <github@hackreactor.com>
2013-08-09 20:39:23 -0700

    created retrieve. have working retrieve and insert

--- src/hashTable.js
+++ src/hashTable.js
@@ -13,20 +13,25 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var i = getIndexBelowMaxForKey(k, this._limit);  //index from the hash function
   console.log(i);
-  if(this._storage.get(i) !== undefined) {
-    this._storage.set(i, [k,v]);
-  } else {
-    var oldValue = this._storage.get(i);
-    var newValue = oldValue.concat([k,v]);
-    this._storage.set(i, newValue);
+  if(this._storage.get(i) === undefined) {  //check if this is the first value at the index of the hashTable
+    this._storage.set(i, [k,v]);  //set an array containing key,value pair at index
+  } else {  //if there is already a key/value pair (if there is a collision)
+    var oldValue = this._storage.get(i);  //get that array of storage, and set oldValue to that array
+    var newValue = oldValue.concat([k,v]);  //concat new key/value pair to oldValue array
+    this._storage.set(i, newValue);  //replace previous array value with newly formed key/value pair array
   }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+  var slot = this._storage.get(i);  //the array of keys/values at given index i
+  for(var j=0; j<slot.length; j=j+2) {
+    if(k === slot[j]) {
+      return slot[j+1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(){ 
