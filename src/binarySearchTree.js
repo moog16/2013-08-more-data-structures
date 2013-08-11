58106a64f0302f3703521ced5e3df60ca46a4ae8
Hack Reactor Students <github@hackreactor.com>
2013-08-10 17:26:52 -0700

    refactored binary tree

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -6,42 +6,29 @@ var makeBinarySearchTree = function(){
   binaryTree.left = null;
 
   binaryTree.insert = function(val) {
-      //debugger;
+
     if(!this.contains(val)) {
-        if(this.value === undefined) { //make a new node at the root, if it is an empty tree
-          this.value = val;
-        } else if(this.value > val) {
-        if(this.left === null) {
-          this.left = makeNewNode(val);
-        } else {
-          this.left.insert(val);
-        }
+
+      if(this.value === undefined) { //make a new node at the root, if it is an empty tree
+        this.value = val;
+      } else if(this.value > val) {
+        this.left === null ? (this.left = makeNewNode(val)) : this.left.insert(val);
       } else if(this.value < val) {
-        if(this.right === null) {
-          this.right = makeNewNode(val);
-        } else {
-          this.right.insert(val);
-        }
+        this.right === null ? (this.right = makeNewNode(val)) : this.right.insert(val);
       }
     }
   };
 
   binaryTree.contains = function(val) {
-    if(val === this.value) {
-      return true;
-    } else if(val < this.value) {
-      if(this.left === null) {
-        return false;
-      } else {
-        return this.left.contains(val);
-      }
-    } else if(val > this.value) {
-      if(this.right === null) {
-        return false;
-      } else {
-        return this.right.contains(val);
-      }
+    var result = false;
+
+    if(val < this.value) {
+      result = this.left && this.left.contains(val) || false;
+    } else {
+      result = this.right && this.right.contains(val) || false;
     }
+    return val === this.value || result;
+    //return val === this.value ||(val < this.value ? (this.left && this.left.contains(val) || false) : (this.right && this.right.contains(val) || false));
   };
 
   binaryTree.depthFirstLog = function(callback) {
